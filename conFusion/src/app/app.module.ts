44198c496eb10c14292b63a77b8257e354a81b2f
Jess Iona Corda <jess.corda@thinkinghut.in>
2018-11-02 15:32:28 +0530

    component 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import {
+   MatToolbarModule,
+   MatListModule
+  } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { AppComponent } from './app.component';
-
 import 'hammerjs';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
     FlexLayoutModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
